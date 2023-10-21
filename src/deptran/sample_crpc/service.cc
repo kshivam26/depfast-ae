59375ef7b66b360973c94519abde12693f56c54f
zoo <test@no.email>
2023-10-21 02:41:29 +0000

    test3

--- src/deptran/sample_crpc/service.cc
+++ src/deptran/sample_crpc/service.cc
@@ -30,10 +30,12 @@ void SampleCrpcServiceImpl::CrpcAdd(const uint64_t& id,
   if (!hasPrinted2) {
       thread_local pid_t t = gettid();
       Log_info("tid of non-leader is %d", t);
-    //   thread_local cpu_set_t cs;
-    //   CPU_ZERO(&cs);
-    //   CPU_SET(i++ / g, &cs);
-    //   verify(sched_setaffinity(t, sizeof(cs), &cs) == 0);
+    //   if (addrChain.size() > 1) {
+    //     thread_local cpu_set_t cs;
+    //     CPU_ZERO(&cs);
+    //     CPU_SET(i++ / g, &cs);
+    //     verify(sched_setaffinity(t, sizeof(cs), &cs) == 0);
+    //   }
       hasPrinted2 = true;  // Update the static variable
   }
 
@@ -64,8 +66,7 @@ void SampleCrpcServiceImpl::BroadcastAdd(const int64_t& value1,
     //   thread_local cpu_set_t cs;
     //   CPU_ZERO(&cs);
     //   CPU_SET(i++ / g, &cs);
-    //   CPU_SET( ((i % 2 == 0) ? i + 1 : i) / g, &cs);
-      verify(sched_setaffinity(t, sizeof(cs), &cs) == 0);
+    //   verify(sched_setaffinity(t, sizeof(cs), &cs) == 0);
       hasPrinted2 = true;  // Update the static variable
   }
 
